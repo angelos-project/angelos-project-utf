c69b43c0c732765e5f496d3b2796e535ab02e4f7
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-12-18 23:43:15 +0100

    Some new tests completed

--- library/src/commonTest/kotlin/org/angproj/utf/UnicodeAwareTest.kt
+++ library/src/commonTest/kotlin/org/angproj/utf/UnicodeAwareTest.kt
@@ -46,6 +46,7 @@ class UnicodeAwareTest : UnicodeAware {
     @Test
     fun testOctetReadWriteStrm() {
         listOf(Policy.passthrough, Policy.escape, Policy.security).forEach { policy ->
+
             var inPos = 0
             var outPos = 0
             val strm1 = Unicode.decode(TestInformationStub.latinLipsum)
@@ -90,44 +91,47 @@ class UnicodeAwareTest : UnicodeAware {
 
     @Test
     fun testOctetReadBlk() {
-        var inPos = 0
-        var outPos = 0
-        val strm1 = Unicode.decode(TestInformationStub.latinLipsum)
-        val outStrm1 = ByteArray(strm1.size)
-        while (inPos < strm1.size) {
-            val cp = readGlyphBlk(strm1.size-inPos) { strm1[inPos++] }
-            writeGlyphBlk(cp, outStrm1.size-outPos) { outStrm1[outPos++] = it }
-        }
-        assertContentEquals(strm1, outStrm1)
+        listOf(Policy.passthrough, Policy.escape, Policy.security).forEach { policy ->
 
-        inPos = 0
-        outPos = 0
-        val strm2 = Unicode.decode(TestInformationStub.latinLipsumEmoji)
-        val outStrm2 = ByteArray(strm2.size)
-        while (inPos < strm2.size) {
-            val cp = readGlyphBlk(strm2.size-inPos) { strm2[inPos++] }
-            writeGlyphBlk(cp, outStrm2.size-outPos) { outStrm2[outPos++] = it }
-        }
-        assertContentEquals(strm2, outStrm2)
+            var inPos = 0
+            var outPos = 0
+            val strm1 = Unicode.decode(TestInformationStub.latinLipsum)
+            val outStrm1 = ByteArray(strm1.size)
+            while (inPos < strm1.size) {
+                val cp = readGlyphByPolicyBlk(strm1.size-inPos, policy) { strm1[inPos++] }
+                writeGlyphByPolicyBlk(cp, outStrm1.size-outPos, policy) { outStrm1[outPos++] = it }
+            }
+            assertContentEquals(strm1, outStrm1)
 
-        inPos = 0
-        outPos = 0
-        val strm3 = Unicode.decode(TestInformationStub.greekLipsum)
-        val outStrm3 = ByteArray(strm3.size)
-        while (inPos < strm3.size) {
-            val cp = readGlyphBlk(strm3.size-inPos) { strm3[inPos++] }
-            writeGlyphBlk(cp, outStrm3.size-outPos) { outStrm3[outPos++] = it }
-        }
-        assertContentEquals(strm3, outStrm3)
+            inPos = 0
+            outPos = 0
+            val strm2 = Unicode.decode(TestInformationStub.latinLipsumEmoji)
+            val outStrm2 = ByteArray(strm2.size)
+            while (inPos < strm2.size) {
+                val cp = readGlyphByPolicyBlk(strm2.size-inPos, policy) { strm2[inPos++] }
+                writeGlyphByPolicyBlk(cp, outStrm2.size-outPos, policy) { outStrm2[outPos++] = it }
+            }
+            assertContentEquals(strm2, outStrm2)
 
-        inPos = 0
-        outPos = 0
-        val strm4 = Unicode.decode(TestInformationStub.chineseLipsum)
-        val outStrm4 = ByteArray(strm4.size)
-        while (inPos < strm4.size) {
-            val cp = readGlyphBlk(strm4.size-inPos) { strm4[inPos++] }
-            writeGlyphBlk(cp, outStrm4.size-outPos) { outStrm4[outPos++] = it }
+            inPos = 0
+            outPos = 0
+            val strm3 = Unicode.decode(TestInformationStub.greekLipsum)
+            val outStrm3 = ByteArray(strm3.size)
+            while (inPos < strm3.size) {
+                val cp = readGlyphByPolicyBlk(strm3.size-inPos, policy) { strm3[inPos++] }
+                writeGlyphByPolicyBlk(cp, outStrm3.size-outPos, policy) { outStrm3[outPos++] = it }
+            }
+            assertContentEquals(strm3, outStrm3)
+
+            inPos = 0
+            outPos = 0
+            val strm4 = Unicode.decode(TestInformationStub.chineseLipsum)
+            val outStrm4 = ByteArray(strm4.size)
+            while (inPos < strm4.size) {
+                val cp = readGlyphByPolicyBlk(strm4.size-inPos, policy) { strm4[inPos++] }
+                writeGlyphByPolicyBlk(cp, outStrm4.size-outPos, policy) { outStrm4[outPos++] = it }
+            }
+            assertContentEquals(strm4, outStrm4)
         }
-        assertContentEquals(strm4, outStrm4)
     }
 }

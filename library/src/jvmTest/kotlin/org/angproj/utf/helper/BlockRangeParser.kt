d97e9c2ca7d9321995944cb65b9fca864b8ba830
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-12-19 16:18:59 +0100

    Some new tests completed

---
+++ library/src/jvmTest/kotlin/org/angproj/utf/helper/BlockRangeParser.kt
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.utf.helper
+
+
+class BlockRangeParser : DataLoader<BlockRange>() {
+
+    private val parser = object : UnicodeDataParser {
+        fun parseLine(line: String): BlockRange {
+            // 0000..007F; Basic Latin
+            val parts = line.split(';')
+            return BlockRange(rangeBounds(parts.get(0)), SearchName(parts.get(1).trim()))
+        }
+    }
+
+    override fun loadData(resourcePath: String): List<BlockRange> {
+        with(parser) {
+            return lineIterator(resourceStream(resourcePath)) { line ->
+                parseLine(line)
+            }
+        }
+    }
+
+    override val allData: List<BlockRange> by lazy {
+        loadData("/Blocks.txt")
+    }
+
+    fun generateBlockRangeEnum(): String {
+        val sortedData = allData.sortedBy { it.searchName.canonical }
+        val sb = StringBuilder()
+        sb.appendLine("package org.angproj.utf.model")
+        sb.appendLine()
+        sb.appendLine("enum class BlockRange(val unicodeBounds: Pair<Int, Int>) {")
+        sortedData.forEachIndexed { idx, data ->
+            val lineEnding = if (idx != allData.lastIndex) "," else ";"
+            sb.appendLine("    ${data.searchName.constant}(\"${data.searchName.canonical}\", \"${data.searchName.klazz}\", \"${data.searchName.klazz}\", 0x${data.unicodeBounds.first.toString(16).uppercase()}..0x${data.unicodeBounds.second.toString(16).uppercase()})$lineEnding")
+        }
+        sb.appendLine("}")
+        return sb.toString()
+    }
+}
---
+++ library/src/jvmTest/kotlin/org/angproj/utf/helper/SearchName.kt
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.utf.helper
+
+data class SearchName(
+    val canonical: String
+) {
+    val slug: String = slugify(canonical)
+    val klazz: String = klazzify(canonical)
+    val constant: String = constantify(canonical)
+
+    companion object {
+        fun slugify(name: String): String {
+            return name.lowercase().replace(" ", "_").replace("-", "_")
+        }
+
+        fun klazzify(name: String): String {
+            return name.replace(" ", "_").replace("-", "_")
+        }
+
+        fun constantify(name: String): String {
+            return name.uppercase().replace(" ", "_").replace("-", "_")
+        }
+    }
+}

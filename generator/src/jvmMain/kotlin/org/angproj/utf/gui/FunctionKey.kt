e1c7a4c3cc1bc115469f3619a0059dbd25324dc0
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2026-01-07 17:21:32 +0100

    Some new ui added

---
+++ generator/src/jvmMain/kotlin/org/angproj/utf/gui/FunctionKey.kt
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2026 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.utf.gui
+
+enum class FunctionKey(val code: Int) {
+    F1(112),
+    F2(113),
+    F3(114),
+    F4(115),
+    F5(116),
+    F6(117),
+    F7(118),
+    F8(119),
+    F9(120),
+    F10(121),
+    F11(122),
+    F12(123)
+}
